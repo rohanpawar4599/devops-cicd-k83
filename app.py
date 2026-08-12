25cbe8fba3c94abdc5429d897261e4bfa01af7d6
rohanpawar4599 <pawar@Ubuntu.myguest.virtualbox.org>
2026-08-12 15:05:15 +0530

    add health endpoints for probe testing

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return "CI/CD with Helm is working with version 2!"
 
+app.route('/health')
+def health():
+    return "OK", 200
+
+@app.route('/health-fail')
+def health_fail():
+    return "FAIL", 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
